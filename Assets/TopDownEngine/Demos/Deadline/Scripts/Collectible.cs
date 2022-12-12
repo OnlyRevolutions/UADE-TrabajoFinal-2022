64c7cad1732bc6070bd90708558405e46defbc33
JMGomezCobo <jmgomezcobo23@gmail.com>
2022-12-12 09:25:07 -0300

    Unity Project Upload

---
+++ Assets/TopDownEngine/Demos/Deadline/Scripts/Collectible.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MoreMountains.TopDownEngine
+{
+	/// <summary>
+	/// A pickable star, that triggers an event if picked, and disables itself if it's been previously collected
+	/// </summary>
+	[AddComponentMenu("TopDown Engine/Items/Collectible")]
+	public class Collectible : Star
+	{
+		/// <summary>
+		/// On Start we disable our star if needed
+		/// </summary>
+		protected override void Start()
+		{
+			base.Start ();
+			DisableIfAlreadyCollected ();
+		}
+
+		/// <summary>
+		/// Disables the star if it's already been collected in the past.
+		/// </summary>
+		protected virtual void DisableIfAlreadyCollected ()
+		{
+			foreach (DeadlineScene scene in ProgressManager.Instance.Scenes)
+			{
+				if (scene.SceneName != SceneManager.GetActiveScene().name) continue;
+				
+				if (scene.CollectedStars.Length < StarID) continue;
+					
+				if (scene.CollectedStars[StarID]) 
+					Disable();
+			}
+		}
+
+		/// <summary>
+		/// Disable this star.
+		/// </summary>
+		protected virtual void Disable()
+		{
+			gameObject.SetActive (false);
+		}
+	}
+}
---
+++ Assets/TopDownEngine/Demos/Deadline/Scripts/DeadlineDoor.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using MoreMountains.Tools;
+using UnityEngine;
+
+namespace MoreMountains.TopDownEngine
+{
+	public class DeadlineDoor : MonoBehaviour
+	{
+
+		public float DoorMovementDuration = 0.3f;
+		public MMTween.MMTweenCurve MovementCurve;
+		public float OpenAngle = 100f;
+
+		protected BoxCollider2D _boxCollider2D;
+		protected Vector3 _vectorA;
+		protected Vector3 _vectorB;
+		protected Vector3 _newAngles;
+		protected bool _rotating = false;
+
+		protected virtual void Start()
+		{
+			Initialization();
+		}
+		
+		protected virtual void Initialization()
+		{
+			_boxCollider2D = gameObject.GetComponent<BoxCollider2D>();
+		}
+		
+		protected void OnTriggerEnter2D(Collider2D other)
+		{
+			if (_rotating)
+			{
+				return;
+			}
+			
+			_newAngles = transform.localEulerAngles;
+			float targetAngle = 0f;
+			float localAngle = transform.localEulerAngles.z;
+			localAngle = (localAngle > 180) ? localAngle - 360 : localAngle;
+			
+			
+			if (localAngle == 0f)
+			{
+				targetAngle = IsLeft(other.transform.position) ? -OpenAngle : OpenAngle;
+			}
+			else
+			{
+				
+				if (localAngle < 0f)
+				{
+					targetAngle = IsLeft(other.transform.position) ? localAngle : 0f;	 
+				}
+				else
+				{
+					targetAngle = IsLeft(other.transform.position) ? 0f : localAngle;
+				}
+			}
+
+			StartCoroutine(RotateDoor(localAngle, targetAngle));
+		}
+
+		protected virtual IEnumerator RotateDoor(float currentAngle, float targetAngle)
+		{
+			_rotating = true;
+			float startTime = Time.time;
+
+			while (Time.time - startTime < DoorMovementDuration)
+			{
+				float elapsed = Time.time - startTime;
+				float newAngle = MMTween.Tween(elapsed, 0f, DoorMovementDuration, currentAngle, targetAngle, MovementCurve);
+				_newAngles.z = newAngle;
+				
+				transform.localEulerAngles = _newAngles;
+				yield return null;
+			}
+			_newAngles.z = targetAngle;
+			transform.localEulerAngles = _newAngles;
+			_rotating = false;
+		}
+		
+		protected virtual bool IsLeft(Vector3 otherPosition)
+		{
+			_vectorA = (transform.position + transform.right * _boxCollider2D.size.x) - transform.position;
+			_vectorB = otherPosition - transform.position;
+			return (-_vectorA.x * _vectorB.y + _vectorA.y * _vectorB.x < 0);
+		}
+	}
+}
