4c254f4e8f6b778c571a33fc5bd7eeb8a75770c9
franrosatti9 <francolezama2@gmail.com>
2022-12-19 20:04:34 -0300

    Mies Impulse ability fixes

--- Assets/UADE-TrabajoFinal/Scripts/ImpulseBounceAbility.cs
+++ Assets/UADE-TrabajoFinal/Scripts/ImpulseBounceAbility.cs
@@ -15,14 +15,21 @@ public class ImpulseBounceAbility : MonoBehaviour
     TopDownController _controller;
     Health _health;
     Rigidbody2D _rb;
+    Collider2D _col;
+
+    float _originalDrag;
+    float _originalAngularDrag;
+    float _originalMass;
 
     public MMFeedbacks HitDamageableFeedback;
+    public MMFeedbacks ImpulseFeedback;
 
     float _impulseTimer;
     public float impulseDuration = 3f;
-    public float impulseSpeed = 2f;
+    public float impulseSpeed = 50f;
 
     public int impulseDamage = 10;
+    [SerializeField] PhysicsMaterial2D bouncyMat;
     
 
 
@@ -34,11 +41,16 @@ public class ImpulseBounceAbility : MonoBehaviour
         _character = GetComponent<Character>();
         _controller = GetComponent<TopDownController>();
         _rb = GetComponent<Rigidbody2D>();
+        _col = GetComponent<Collider2D>();
         _characterMovement = _character?.FindAbility<CharacterMovement>();
         _characterDash = _character?.FindAbility<CharacterDamageDash2D>();
         _orientation = _character?.FindAbility<CharacterOrientation2D>();
         _characterWeapon = _character?.FindAbility<CharacterHandleWeapon>();
         _health = _character.CharacterHealth;
+
+        _originalAngularDrag = _rb.angularDrag;
+        _originalDrag = _rb.drag;
+        _originalMass = _rb.mass;
 
 
     }
@@ -56,35 +68,59 @@ public class ImpulseBounceAbility : MonoBehaviour
         if (_usingAbility || _characterDash.Dashing) return;
         _usingAbility = true;
         _health.DamageDisabled();
+        _health.ImmuneToKnockback = true;
+        _rb.mass = 0.1f;
         EnableAbilities(false);
 
         _controller.FreeMovement = false;
 
-        _rb.useFullKinematicContacts = true;
-        _controller.SetKinematic(true);
+        //_rb.useFullKinematicContacts = true;
+        // _controller.SetKinematic(true);
+        _col.sharedMaterial = bouncyMat;
+
+        ImpulseFeedback?.PlayFeedbacks();
 
         //_startingDir = (transform.position + _characterWeapon.WeaponAimComponent.CurrentAim).normalized;
         _impulseDir =  ((transform.position + _characterWeapon.WeaponAimComponent.CurrentAim) - transform.position).normalized;
-        if (!_orientation.IsFacingRight) _impulseDir *= -1f; 
+        if (!_orientation.IsFacingRight) _impulseDir *= -1f;
+
+        _rb.drag = 0;
+        _rb.angularDrag = 0;
+        _rb.AddForce(_impulseDir * impulseSpeed, ForceMode2D.Impulse);
+
+
+        
         
     }
 
     public void StopImpulse()
     {
+        _rb.velocity = Vector3.zero;
         _impulseTimer = 0;
         _usingAbility = false;
-        _health.DamageDisabled();
+        _health.DamageEnabled();
+        _health.ImmuneToKnockback = false;
+        _rb.mass = _originalMass;
         EnableAbilities(true);
 
         _controller.FreeMovement = true;
 
+        _col.sharedMaterial = null;
+
+        ImpulseFeedback?.StopFeedbacks();
+
         _impulseDir = Vector3.zero;
-        _controller.SetKinematic(false);
+        //_controller.SetKinematic(false);
+
+        _rb.drag = _originalDrag;
+        _rb.angularDrag = _originalAngularDrag;
+
     }
 
     public void EnableAbilities(bool enabled)
     {
         _characterMovement.InputAuthorized = enabled;
+        _characterMovement.AbilityPermitted = enabled;
         _characterDash.AbilityPermitted = enabled;
         _characterWeapon.AbilityPermitted = enabled;
     }
@@ -96,7 +132,8 @@ public class ImpulseBounceAbility : MonoBehaviour
             if (_impulseTimer < impulseDuration)
             {
                 _impulseTimer += Time.deltaTime;
-                _controller.MovePosition((transform.position + _impulseDir));
+                if (_rb.velocity.magnitude > 5f) _rb.velocity = _rb.velocity.normalized * 5f;
+                //_controller.MovePosition((transform.position + _impulseDir));
                 
             }
             else StopImpulse();
@@ -109,7 +146,7 @@ public class ImpulseBounceAbility : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawLine(transform.position, transform.position + _characterWeapon.WeaponAimComponent.CurrentAim.normalized);
         Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(transform.position, transform.position + _impulseDir);
+        Gizmos.DrawLine(transform.position, transform.position + _impulseDir * 3f);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -120,13 +157,16 @@ public class ImpulseBounceAbility : MonoBehaviour
         {
             if(!colHealth.ImmuneToDamage)
             {
-                HitDamageableFeedback?.PlayFeedbacks();
                 colHealth.Damage(impulseDamage, gameObject, 0.1f, 0.5f, Vector3.zero);
+                if (colHealth.gameObject.layer == 13)
+                {
+                    HitDamageableFeedback?.PlayFeedbacks();
+                }
             }
         }
-        if(collision.gameObject.layer == 8) // Si colisiona con una pared u obstaculo
+        /*if(collision.gameObject.layer == 8) // Si colisiona con una pared u obstaculo
         {
             _impulseDir = Vector3.Reflect(_impulseDir, collision.GetContact(0).normal);
-        }
+        }*/
     }
 }
